0ee7ce82843192ccb141a025f51af5aee1451f49
Tyler Williams <williams.tyler@gmail.com>
2024-01-22 15:18:08 -0800

    Open the index, but don't do anything yet

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,7 +5,12 @@ import (
 	"flag"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"runtime"
 
+	"github.com/cockroachdb/pebble"
+	"github.com/google/codesearch/index"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
@@ -15,22 +20,48 @@ import (
 )
 
 var (
-	listen = flag.String("listen", ":2633", "Address and port to listen on")
+	listen   = flag.String("listen", ":2633", "Address and port to listen on")
+	indexDir = flag.String("index_dir", "", "Directory to store index in. Default: '~/.csindex/'")
 )
 
 type codesearchServer struct {
-	//	csspb.UnimplementedCodesearchServiceServer
+	db *pebble.DB
 }
 
-func New() *codesearchServer {
-	return &codesearchServer{}
+func defaultDir() string {
+	var home string
+	home = os.Getenv("HOME")
+	if runtime.GOOS == "windows" && home == "" {
+		home = os.Getenv("USERPROFILE")
+	}
+	return filepath.Clean(home + "/.csindex")
 }
+
+func New() (*codesearchServer, error) {
+	d := *indexDir
+	if d == "" {
+		d = defaultDir()
+	}
+	db, err := pebble.Open(d, &pebble.Options{})
+	if err != nil {
+		return nil, err
+	}
+	return &codesearchServer{
+		db: db,
+	}, nil
+}
+
 func (css *codesearchServer) Index(ctx context.Context, req *inpb.IndexRequest) (*inpb.IndexResponse, error) {
 	log.Printf("Index RPC")
+	iw := index.Create(css.db)
+	_ = iw
 	return &inpb.IndexResponse{}, nil
 }
+
 func (css *codesearchServer) Search(ctx context.Context, req *srpb.SearchRequest) (*srpb.SearchResponse, error) {
 	log.Printf("Search RPC")
+	ir := index.Open(css.db)
+	_ = ir
 	return &srpb.SearchResponse{}, nil
 }
 
@@ -40,7 +71,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	css := New()
+	css, err := New()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	grpcServer := grpc.NewServer()
 	reflection.Register(grpcServer)
